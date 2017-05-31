1084f672cb69164048c3b31aa5d0cf6898fcd9ab
z13711406856 <vzhanyao911010@.gzps.corp.csg>
2017-05-31 17:35:42 +0800

    2017-05-31

--- src/com/boz/action/LoginAction.java
+++ src/com/boz/action/LoginAction.java
@@ -1,11 +1,17 @@
 package com.boz.action;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,8 +27,21 @@ import com.boz.obj.User;
 public class LoginAction {
 	@RequestMapping("/login")
 	@ResponseBody
-	public String login(@Valid User user,HttpServletRequest request){
-
+	public String login(@Valid User user,BindingResult bindingResult){
+		if(bindingResult.hasErrors()){
+			List<ObjectError> l=bindingResult.getAllErrors();
+			for(ObjectError objectError:l){
+				FieldError fieldError=(FieldError) objectError;
+				Object s=fieldError.getRejectedValue();
+				Object[] objects=objectError.getArguments();
+				DefaultMessageSourceResolvable defaultMessageSourceResolvable=(DefaultMessageSourceResolvable)objects[0];
+				defaultMessageSourceResolvable.getDefaultMessage();
+				String[] codes=objectError.getCodes();
+				String message=objectError.getDefaultMessage();
+				String objectName=objectError.getObjectName();
+				String toString=objectError.toString();
+			}
+		}
 		throw new RuntimeException("123");
 	}
 	@RequestMapping("/ModelAndView")
--- src/com/boz/obj/User.java
+++ src/com/boz/obj/User.java
@@ -1,8 +1,12 @@
 package com.boz.obj;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 public class User {
+	@Size(min=4,max=10,message="长度不能小于4,不能大于10")
+	@Pattern(regexp="abd",message="必须包含abc字符")
 	private String account;
 	@NotNull(message="密码不能为空")
 	private String password;
