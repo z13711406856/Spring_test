ec8fdb47b706f28db2eb76a52cf6cfbe3a23b864
z13711406856 <vzhanyao911010@.gzps.corp.csg>
2017-06-09 14:23:34 +0800

    20170609

--- src/com/boz/aspect/RequestMappingAspect.java
+++ src/com/boz/aspect/RequestMappingAspect.java
@@ -26,15 +26,7 @@ public class RequestMappingAspect {
 		long begin=System.currentTimeMillis();
 		Object result=null;
 		Object[] objects=jp.getArgs();//get request arguments
-		for(Object obj:objects){
-			if(obj instanceof BindingResult){
-				BindingResult bindingResult=(BindingResult) obj;
-				if(bindingResult.hasErrors()){
-					result=doValidError(bindingResult);
-					break;
-				}
-			}
-		}
+		result=doBindingResult(objects);
 		if(result==null){
 			result=jp.proceed(jp.getArgs());
 		}
@@ -43,12 +35,26 @@ public class RequestMappingAspect {
 		return result;
 	}
 	
+	private Object doBindingResult(Object[] args){//遍历每个请求参数，找到BindingResult
+		for(Object obj:args){
+			if(obj instanceof BindingResult){
+				BindingResult bindingResult=(BindingResult) obj;
+				if(bindingResult.hasErrors()){
+					return doValidError(bindingResult);
+				}
+			}
+		}
+		return null;
+	}
+	//将异常数据添加到Map中,以json格式返回
+	/*{"type":"vaildError",
+	  "content":[{"password":"密码不能为空"}]} 此[]内的列表可以存储多个错误提示*/
 	private Object doValidError(BindingResult bindingResult){
-		String typeName="vaildError";
+		String typeValue="vaildError";
 		String typeKey="type";
 		String listKey="content";
 		Map<String, Object> map=new HashMap<String, Object>();
-		map.put(typeKey, typeName); 
+		map.put(typeKey, typeValue); 
 		List<Map.Entry<String, String>> cList=new ArrayList<Map.Entry<String,String>>();
 		map.put(listKey, cList);
 		List<ObjectError> eList=bindingResult.getAllErrors();
